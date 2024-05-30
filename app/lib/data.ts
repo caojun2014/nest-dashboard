d47ea22fe102efeb528197e8e6019c936ca0cbff
caojun <981718022@qq.com>
2024-05-30 23:35:29 +0800

    "data create update“

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -91,7 +91,7 @@ export async function fetchCardData() {
   noStore();
 }
 
-const ITEMS_PER_PAGE = 6;
+const ITEMS_PER_PAGE = 12;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
